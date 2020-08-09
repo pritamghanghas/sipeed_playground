bc0b61651657c8ee47e192de7e0bdf590e6bc917
Pritam Ghanghas <pritam.ghanghas@gmail.com>
2020-08-09 17:23:57 +0530

    added documentation of GPIO pin

--- hcsr04.py
+++ hcsr04.py
@@ -19,8 +19,8 @@ class HCSR04:
     # echo_timeout_us is based in chip range limit (400cm)
     def __init__(self, gpio_func_trig, gpio_func_echo, echo_timeout_us=500*2*30):
         """
-        trigger_pin: Output pin to send pulses
-        echo_pin: Readonly pin to measure the distance. The pin should be protected with 1k resistor
+        gpio_func_trig: Output pin to send pulses of type GPIO.****
+        gpio_func_echo: Readonly pin to measure the distance. The pin should be protected with 1k resistor. type: GPIO.***
         echo_timeout_us: Timeout in microseconds to listen to echo pin. 
         By default is based in sensor limit range (4m)
         """
